5ed6ed40d8f0146498036d31bfd2810e93bc07d7
LeBarney <barneywarframe@gmail.com>
2021-02-16 15:56:59 +0100

    premier exo js sur le "trading"

---
+++ trading.js
@@ -0,0 +1,9 @@
+let prix_du_btc = 8452
+
+if (prix_du_btc > 20000) {
+  console.log("vendre")
+} else if (prix_du_btc < 10000) {
+  console.log("acheter")
+} else {
+  console.log("ne rien faire")
+}
